25a05ca3731a5097c51941adf46b216d2680c6ed
Spriha Mandal <spriha2103@gmail.com>
2025-09-30 01:38:05 +0530

    fixes

--- src/projects/customScreen.jsx
+++ src/projects/customScreen.jsx
@@ -9,17 +9,16 @@ export const CustomScreen = () => {
 
     return (
         <div
-            className={`w-screen h-screen ${isDark ? "bg-neutral-800 text-white" : "bg-white text-black"} flex items-center justify-center`}
+            className={`w-screen h-screen ${isDark ? "bg-neutral-800 text-white" : "bg-white text-black"} flex flex-col items-center justify-center text-center`}
         >
+            <h1>Hello World</h1>
+            <h1>This is custom screen</h1>
+
             <button onClick={toggleTheme} className={`plain-button ${isDark ? "dark-button" : "light-button"}`}>{themeText}</button>
 
 
 
-            {/* <div className=" flex items-center">
 
-                <CryptoScreen />
-
-            </div> */}
 
         </div>
     );
